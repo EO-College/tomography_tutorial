415cf0a0b062e72eae23afd7d51f06e2ca23485b
johntruckenbrodt <john.truckenbrodt@uni-jena.de>
2018-05-04 15:06:19 +0200

    added naploean extension and parametrization

--- docs/conf.py
+++ docs/conf.py
@@ -53,7 +53,20 @@ extensions = [
     'sphinx.ext.doctest',
     'sphinx.ext.coverage',
     'sphinx.ext.imgmath',
+    'sphinx.ext.napoleon',
 ]
+
+napoleon_google_docstring = False
+napoleon_numpy_docstring = True
+napoleon_include_init_with_doc = False
+napoleon_include_private_with_doc = False
+napoleon_include_special_with_doc = True
+napoleon_use_admonition_for_examples = False
+napoleon_use_admonition_for_notes = False
+napoleon_use_admonition_for_references = False
+napoleon_use_ivar = False
+napoleon_use_param = True
+napoleon_use_rtype = True
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']

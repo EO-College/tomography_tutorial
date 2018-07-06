6ced139c5f288dd5044f2dfe490464faa823bc73
John Truckenbrodt <john.truckenbrodt@uni-jena.de>
2018-07-06 19:49:36 +0200

    [setup] minor description edits

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ import os
 import shutil
 
 # Create .tomography in HOME - Directory
-directory = os.path.join(os.path.expanduser("~"), '.tomography')
+directory = os.path.join(os.path.expanduser('~'), '.tomography')
 
 if not os.path.exists(directory):
     os.makedirs(directory)
@@ -28,5 +28,5 @@ setup(name='tomography',
       license='MIT',
       zip_safe=False)
 
-# copy the tutorial notebook the
+# copy the tutorial notebook from the directory of the installed package to .tomography
 shutil.copyfile('./tomography/tutorial.ipynb', os.path.join(directory, 'tutorial.ipynb'))
